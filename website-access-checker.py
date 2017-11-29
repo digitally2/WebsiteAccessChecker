9c310fdac2b10252b11e429ce2629a1e9ab9a26d
Mike Zipp <mzipp@ext.uber.com>
2017-11-29 13:10:48 -0800

    sanitized and tested

--- website-access-checker.py
+++ website-access-checker.py
@@ -1,13 +1,21 @@
 import urllib
+import csv
+
+list_of_websites = []
 
 
-list_of_websites_ext = ["http://www.google.com", "https://www.google.com"]
-list_of_websites_int = ["http://www.google.com", "https://www.google.com"]
+
+#UPDATE LISTS
+def update_lists():
+   with open("website-list.tsv", "rU") as csvfile:
+      reader = csv.reader(csvfile, dialect='excel-tab')
+      for row in reader:
+         list_of_websites.append(row[0])
+update_lists()
 
 
-
-def external():
-   for website in list_of_websites_ext:
+def test_websites_from_list():
+   for website in list_of_websites:
       returned_code = urllib.urlopen(website).getcode()
       if returned_code == 200:
          print "GOOD |  %s" % (website)
@@ -15,26 +23,21 @@ def external():
          print "FAIL | %s" % (website)
          
 
-def internal():
-   for website in list_of_websites_int:
-      returned_code = urllib.urlopen(website).getcode()
-      if returned_code == 200:
-         print "GOOD |  %s" % (website)
-      else:
-         print "FAIL | %s" % (website)
-
+def print_list_of_websites():
+   for website in list_of_websites:
+      print website
 
 
 def main():
-   option = raw_input("\n1 - Test Internal Websites\n2 - Test External Websites\n>")
+   option = raw_input("\n1 - Test list of websites\n2 - Print list of websites\n>")
 
    if option == "1":
       print "you selected 1"
-      internal()
+      test_websites_from_list()
 
    if option == "2":
       print "you selected 2"
-      external()
+      print_list_of_websites()
 
 
 main()
